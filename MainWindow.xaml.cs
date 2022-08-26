964624ce24447ffbb34c9c15b5c34927533c9c8d
Niels Schuler <Schuler@wehrle-werk.de>
2022-08-26 10:40:19 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace PasswortTSLock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string installPassword = "WehrleCoolInstall";
+        private int trys = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkPassword_Click(object sender, RoutedEventArgs e)
+        {
+            string password = passwordBox.Password.ToString();
+
+            if (password.Equals(installPassword)){
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                passwordLabel.Content = $"{trys}. Wrong Password";
+                trys++;
+            }
+        }
+    }
+}
